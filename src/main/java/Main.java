6795fb98cf0737cfcab6bcd96b2304fff877873e
vuviethoang414 <hoangvu04012004@gmail.com>
2025-01-16 14:15:10 +0700

    Bai Tap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,16 @@ public class Main {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
         // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+        int myInt = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, Java!";
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myString);
+        if(myInt > 10){
+            System.out.println("Greater than 10");
+        }else{
+            System.out.println("Less or equal to 10");
+        }
     }
 }
